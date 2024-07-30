aa50b996138b06d964d0511674b8f24dd282f938
Bhavyadoshi266 <doshibhavy0712@gmail.com>
2024-07-31 00:17:36 +0530

    created two nwe section in Department.jsx and worked on it

--- src/MyComponents/All_doctors.jsx
+++ src/MyComponents/All_doctors.jsx
@@ -34,7 +34,7 @@ export default function All_doctors() {
             <div className="all_doctors">
                 <div className="wrapper">
                     <div className="all_doctor d-flex justify-content-between align-items-center mb-5">
-                        <p className='d-flex justify-content-center align-items-center Corosel_to_aboutus_header animate-text' style={{ color: "#3a6cee" }}>Doctors in our clinic</p>
+                        <p className='d-flex justify-content-center align-items-center Corosel_to_aboutus_header animate-text' style={{ color: "#222222",fontWeight:'500' }}>Doctors in our clinic</p>
                         <a href="#" className='p-0 m-0'>
                             <button className='btn_aboutus animate-text py-3 px-5'>ALL DOCTORS</button>
                         </a>
--- src/MyComponents/Department.jsx
+++ src/MyComponents/Department.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import All_doctors from './All_doctors';
 import departmentone from './Images/departmentone.jpg'
 import departmenttwo from './Images/departmenttwo.jpg'
 import departmentthree from './Images/departmentthree.jpg'
@@ -41,6 +42,29 @@ export default function Department() {
             departmentname: 'Nephrology',
             departmentdesc: 'Our nephrology department is dedicated to the diagnosis, treatment, and management of kidney-related conditions. From accurate diagnoses to personalized treatment plans and ongoing support.',
             departmentimg: departmentsix,
+        },
+    ]
+
+    const work_process = [
+        {
+            id: 1,
+            processname: 'Appointment Scheduling',
+            processdesc: 'Patients can easily schedule appointments through our website, mobile app, or by calling our office.',
+        },
+        {
+            id: 2,
+            processname: 'Consultation and Assessment',
+            processdesc: 'Our healthcare providers review your medical history and discuss any current health concerns.',
+        },
+        {
+            id: 3,
+            processname: 'Diagnosis and Treatment Planning',
+            processdesc: 'Based on the diagnosis, we develop a customized treatment plan tailored to your specific needs.',
+        },
+        {
+            id: 4,
+            processname: 'Follow-Up and Monitoring',
+            processdesc: 'We provide continuous support and guidance to ensure your health improves and any concerns are promptly addressed.',
         },
     ]
 
@@ -104,7 +128,73 @@ export default function Department() {
                             ))}
                         </div>
                     </div>
+
+                    <div className="work_process .container">
+                        <div className="row ">
+                            <div className="col-11 work_process_left">
+                                <p className='Corosel_to_aboutus_header animate-text' style={{ color: "#222222", fontWeight: '500' }}>Our work process at Meddoc</p>
+                                <p className='processdesc'>At Meddoc, we follow a comprehensive and patient-centered work process to ensure the highest quality of care.</p>
+                            </div>
+                        </div>
+                        <div className="col-12 work_process_right">
+                            {work_process.map((item) => (
+                                <div className='work_process_container d-flex'>
+                                    <div className="col-1 d-flex justify-content-center processno   ">
+                                        <p className='processid'>{item.id}</p>
+                                    </div>
+                                    <div className="col-10 work_process_container_steps">
+                                        <p className='processname'>{item.processname}</p>
+                                        <p className='processdesc'>{item.processdesc}</p>
+                                    </div>
+
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
+
+             
+
                 </div>
+                <All_doctors />
+
+                {/* <div className="join_team">
+                    <div className="join_team_img">
+                        <div className="wrapper .container">
+                            <div className="row join_team_container">
+                                <div className="col-7">
+                                    <p style={{fontSize:"48px",color:'#ffffff',lineHeight:'1.2em'}}>Join our Meddoc team and help make the world a better place!</p>
+                                    <p className='col-9' style={{fontSize:"16px",color:'#ffffff'}}>We believe in the power of teamwork and collaboration to provide comprehensive care tailored to each individual's needs.</p>
+                                </div>
+
+                                <div className="col-12">
+                                    <a href="" className='join_teamlink'>JOIN US</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+
+
+                <div className="join_team">
+                    <div className="join_team_img">
+                        <div className="wrapper container">
+                            <div className="row join_team_container ">
+                                <div className="col-lg-7 col-md-12 mb-3">
+                                    <p style={{ fontSize: "48px", color: '#ffffff', lineHeight: '1.2em' }}>Join our Meddoc team and help make the world a better place!</p>
+                                    <p className='col-lg-9' style={{ fontSize: "16px", color: '#ffffff' }}>We believe in the power of teamwork and collaboration to provide comprehensive care tailored to each individual's needs.</p>
+                                </div>
+                                <div className="col-lg-12 col-md-12">
+                                    <a href="" className='join_teamlink'>JOIN US</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+
+
             </section>
         </>
     )
