6dafb1db041a491652603d0b4298186e56829fb1
Bhavyadoshi266 <doshibhavy0712@gmail.com>
2024-08-08 00:32:08 +0530

    completed one class

--- src/MyComponents/Contact_us.jsx
+++ src/MyComponents/Contact_us.jsx
@@ -12,7 +12,46 @@ export default function Contact_us() {
                     </div>
                 </div>
 
-                
+                <div className="wrapper">
+                    {/* <div className="conatct_details .container">
+                        <div className="row d-flex justify-content-between align-items-start">
+                            <div className="col-4 conatct_detail" style={{borderRight: "1px solid rgb(255,255,255,0.2)"}}>
+                                <p className='m-0'><i class="fa-solid fa-location-dot"></i></p>
+                                <a href="https://www.google.com/maps?sca_esv=555467477&amp;output=search&amp;q=1333+2nd+Ave,+Seattle,+WA+98118&amp;source=lnms&amp;entry=mc&amp;sa=X&amp;ved=2ahUKEwjJ5ZrgrtKAAxVszgIHHQx-AjwQ0pQJegQICxAB" target="_blank" class="conatct_detail_text">4517 Washington Ave. Manchester, Kentucky 39495</a>
+                            </div>
+                            <div className="col-4 conatct_detail center_borders">
+                                <p className='m-0'><i class="fa-solid fa-envelope"></i></p>
+                                <a className='m-0 conatct_detail_text'  href="mailto:meddoc@simple.com" >meddoc@simple.com</a>
+                            </div>
+                            <div className="col-4 conatct_detail" style={{borderLeft: "1px solid rgb(255,255,255,0.2)"}}>
+                                <p className='m-0'><i class="fa-solid fa-phone"></i></p>
+                                <a className='m-0 conatct_detail_text' href="tel:(302)5550107">(302) 555 0107</a>
+                                <a className='m-0 conatct_detail_text' href="tel:(684) 555 0102">(684) 555 0102</a>
+                            </div>
+                        </div>
+                    </div> */}
+
+                    <div className="conatct_details container">
+                        <div className="row d-flex justify-content-between align-items-start">
+                            <div className="col-12 col-md-4 conatct_detail mb-3 mb-md-0" style={{ borderRight: "1px solid rgba(255, 255, 255, 0.2)" }}>
+                                <p className="m-0"><i className="fa-solid fa-location-dot"></i></p>
+                                <a href="https://www.google.com/maps?sca_esv=555467477&amp;output=search&amp;q=1333+2nd+Ave,+Seattle,+WA+98118&amp;source=lnms&amp;entry=mc&amp;sa=X&amp;ved=2ahUKEwjJ5ZrgrtKAAxVszgIHHQx-AjwQ0pQJegQICxAB" target="_blank" rel="noopener noreferrer" className="conatct_detail_text">4517 Washington Ave. Manchester, Kentucky 39495</a>
+                            </div>
+                            <div className="col-12 col-md-4 conatct_detail mb-3 mb-md-0 center_borders">
+                                <p className="m-0"><i className="fa-solid fa-envelope"></i></p>
+                                <a className="m-0 conatct_detail_text" href="mailto:meddoc@simple.com">meddoc@simple.com</a>
+                            </div>
+                            <div className="col-12 col-md-4 conatct_detail" style={{ borderLeft: "1px solid rgba(255, 255, 255, 0.2)" }}>
+                                <p className="m-0"><i className="fa-solid fa-phone"></i></p>
+                                <a className="m-0 conatct_detail_text" href="tel:(302)5550107">(302) 555 0107</a>
+                                <a className="m-0 conatct_detail_text" href="tel:(684) 555 0102">(684) 555 0102</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
 
 
 
